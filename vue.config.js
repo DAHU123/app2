ad9eea0d2553b6a8ae8b84de04614237fdb89918
Chenlili <986220912@qq.com>
2020-09-06 18:46:52 +0800

    完善项目

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 const confignation = require('./package.json')
 module.exports = {
     // 告诉子应用在这个地址加载静态资源，否则会去基座应用的域名下加载
-    publicPath: '//localhost:2324',
+    publicPath: '//localhost:8082',
     // 开发服务器
     devServer: {
-        port: 2324
+        port: 8082
     },
     configureWebpack: {
         // 导出umd格式的包，在全局对象上挂载属性package.name，基座应用需要通过这个全局对象获取一些信息，比如子应用导出的生命周期函数
